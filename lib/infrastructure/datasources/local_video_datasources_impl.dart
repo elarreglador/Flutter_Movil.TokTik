c2007ecfde0cfe8a0bbec208ef9f04eae876ce54
David M <elarreglador@protonmail.com>
2024-04-11 07:37:43 +0200

    Datasouruce, repositorios e implementaciones

---
+++ lib/infrastructure/datasources/local_video_datasources_impl.dart
@@ -0,0 +1,25 @@
+import 'package:toktik/domain/datasources/video_posts_datasource.dart';
+import 'package:toktik/domain/entities/video_post.dart';
+import 'package:toktik/infrastructure/models/local_video_model.dart';
+import 'package:toktik/shared/data/local_video_posts.dart';
+
+class LocalVideosDatasource implements VideoPostDatasource{
+  @override
+  Future<List<VideoPost>> getFavoriteVideosByUser(String userID) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<VideoPost>> getTrendingVideosByPage(int page) async {
+
+    // Simula tiempo de espera de peticion de datos
+    await Future.delayed( const Duration (seconds: 2));
+
+    final List<VideoPost> newVideos = videoPosts.map(
+      (video) => LocalVideoModel.fromJson(video).toVideoPostEntity()
+    ).toList();
+    
+    return newVideos;
+  }
+
+}
---
+++ lib/infrastructure/repositories/video_posts_repository_impl.dart
@@ -0,0 +1,23 @@
+import 'package:toktik/domain/datasources/video_posts_datasource.dart';
+import 'package:toktik/domain/entities/video_post.dart';
+import 'package:toktik/domain/repositories/video_posts_repository.dart';
+
+abstract class VideoPostsRepository implements VideoPostRepository{
+
+  final VideoPostDatasource videosDatasource; 
+
+  VideoPostsRepository({
+    required this.videosDatasource
+    });
+
+  @override
+  Future<List<VideoPost>> getFavoriteVideosByUser( String userID ){
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<VideoPost>> getTrendingVideosByPage( int page ){
+    return videosDatasource.getTrendingVideosByPage(page);
+  }
+
+}
